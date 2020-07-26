0afb29d97a3f6e379d6111edf27d3ab3b58e9b33
vikram741 <vikramvikram741741@gmail.com>
2020-07-26 15:00:19 +0530

    afn

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+
+  add_book = true;
+  show_request = false;
   constructor() { }
+
+  categories = [
+    "Fantasy",
+"Adventure",
+"Romance",
+"Contemporary",
+"Dystopian",
+"Mystery",
+"Horror",
+"Thriller",
+"Paranormal",
+"Historical fiction",
+"Science Fiction",
+"Memoir",
+"Motivational",
+"Health",
+"History",
+"Travel",
+"Humor",
+"Children’s",
+  ]
+  languages = [ "Hindi","Telugu","English","Tamil","Marathi","Bengali" ]
 
   ngOnInit() {
   }
+
+  addBook()
+  {
+
+  }
+
+  addedCat = new Set<number>();
+  addCat(x){
+      this.addedCat.add(x);
+  }
+  removeCat(x)
+  {
+    this.addedCat.delete(x)
+  }
+
+
 
 }
